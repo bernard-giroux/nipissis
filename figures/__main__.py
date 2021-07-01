46aecd6c8c0864f19866dd75cd060ebd22fc9d82
CloudyOverhead <jerome.simon@geolearn.ca>
2021-07-01 16:51:51 -0400

    Add arguments to entrypoint

--- figures/__main__.py
+++ figures/__main__.py
@@ -1,4 +1,18 @@
+from argparse import ArgumentParser
+
 from figures import *
 from catalog import catalog
+
+
+parser = ArgumentParser()
+parser.add_argument('--metadata', '-m', action='store_true')
+parser.add_argument('--figure', '-f', type=str)
+args = parser.parse_args()
+
+if args.metadata:
+    if args.figure:
+        catalog.regenerate(args.figure)
+    else:
+        catalog.regenerate_all()
 
 catalog.draw_all(show=False)
