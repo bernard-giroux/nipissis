819d008775e8aad444a36eb534bd843594c2f90b
CloudyOverhead <jerome.simon@geolearn.ca>
2021-07-01 11:46:00 -0400

    Add entrypoint for generating figures

---
+++ figures/__init__.py
@@ -0,0 +1,3 @@
+from . import analyze_dependency
+from . import dependencies
+from . import mk_hist
---
+++ figures/__main__.py
@@ -0,0 +1,4 @@
+from figures import *
+from catalog import catalog
+
+catalog.draw_all(show=False)
