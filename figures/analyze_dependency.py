e767f4e7da793a6adeb5ae0f0daac6773bb24d04
CloudyOverhead <jerome.simon@geolearn.ca>
2021-07-01 11:59:42 -0400

    Use catalog for exploratory data analysis

--- figures/analyze_dependency.py
+++ figures/analyze_dependency.py
@@ -3,73 +3,90 @@
 import pickle as pkl
 
 import pandas as pd
-import matplotlib as mpl
 from matplotlib import pyplot as plt
+
+from catalog import catalog, Figure, Metadata
 
 DISTANCES_SITES = {0: 93.02, 1: 739.80, 2: 537.42}
 
 
-train_data = pd.read_excel('donnees_ironore.xlsx')
+class Metadata(Metadata):
+    def generate(self):
+        data = pd.read_excel('donnees_ironore.xlsx')
 
-rms_val = pkl.load(open("rms_val.pkl", "rb"))
-train_data["RMS"] = rms_val
+        rms_val = pkl.load(open("rms_val.pkl", "rb"))
+        data["RMS"] = rms_val
 
-for i in range(70):
-    if i < 21:
-        distance = DISTANCES_SITES[0]
-    elif i < 45:
-        distance = DISTANCES_SITES[1]
-    else:
-        distance = DISTANCES_SITES[2]
-    train_data.loc[i, "Distance"] = distance
+        for i in range(70):
+            if i < 21:
+                distance = DISTANCES_SITES[0]
+            elif i < 45:
+                distance = DISTANCES_SITES[1]
+            else:
+                distance = DISTANCES_SITES[2]
+            data.loc[i, "Distance"] = distance
 
-train_data = train_data[
-    ["No Train", "RMS", "Distance", "Vitesse (mph)", "Poids (Tonnes)"]
-]
-train_data.columns = ["Train", "RMS", "Distance", "MPH", "Poids"]
-train_data.drop(65)
-train_data["MPH"] = train_data["MPH"].str.replace(" mph", "")
-train_data["MPH"] = train_data["MPH"].str.replace(" mi/h", "")
-train_data["MPH"] = train_data["MPH"].astype(float)
-train_data = train_data[~pd.isna(train_data["MPH"])]
+        data = data[
+            ["No Train", "RMS", "Distance", "Vitesse (mph)", "Poids (Tonnes)"]
+        ]
+        data.columns = ["Train", "RMS", "Distance", "MPH", "Poids"]
+        data.drop(65)
+        data["MPH"] = data["MPH"].str.replace(" mph", "")
+        data["MPH"] = data["MPH"].str.replace(" mi/h", "")
+        data["MPH"] = data["MPH"].astype(float)
+        data = data[~pd.isna(data["MPH"])]
 
-with pd.option_context('display.max_rows', None, 'display.max_columns', None):
-    print(train_data)
+        # with pd.option_context(
+        #     'display.max_rows', None, 'display.max_columns', None,
+        # ):
+        #     print(data)
 
-mpl.rc('font', size=20)
+        for column in data.columns:
+            self[column] = data[column].values
 
-plt.figure(figsize=(12, 8))
-plt.scatter(train_data["Distance"], train_data["RMS"], s=12, c="k")
-plt.xlabel("Distance [m]")
-plt.ylabel("Amplitude des vibrations [mm/s]")
-plt.suptitle("a)")
-plt.grid(True, which='major', color='k', alpha=.35)
-plt.grid(True, which='minor', linestyle='--', color='k', alpha=.1)
-plt.minorticks_on()
-plt.tight_layout(rect=[0, 0, 1, 0.95])
-plt.savefig("distance.png")
-plt.show()
 
-plt.figure(figsize=(12, 8))
-plt.scatter(train_data["MPH"], train_data["RMS"], s=12, c="k")
-plt.xlabel("Vitesse [mph]")
-plt.ylabel("Amplitude des vibrations [mm/s]")
-plt.suptitle("b)")
-plt.grid(True, which='major', color='k', alpha=.35)
-plt.grid(True, which='minor', linestyle='--', color='k', alpha=.1)
-plt.minorticks_on()
-plt.tight_layout(rect=[0, 0, 1, 0.95])
-plt.savefig("velocity.png")
-plt.show()
+class Distance(Figure):
+    Metadata = Metadata
 
-plt.figure(figsize=(12, 8))
-plt.scatter(train_data["Poids"], train_data["RMS"], s=12, c="k")
-plt.xlabel("Poids [tonnes]")
-plt.ylabel("Amplitude des vibrations [mm/s]")
-plt.suptitle("c)")
-plt.grid(True, which='major', color='k', alpha=.35)
-plt.grid(True, which='minor', linestyle='--', color='k', alpha=.1)
-plt.minorticks_on()
-plt.tight_layout(rect=[0, 0, 1, 0.95])
-plt.savefig("weight.png")
-plt.show()
+    def plot(self, data):
+        plt.scatter(data["Distance"], data["RMS"], s=12, c="k")
+        plt.xlabel("Distance [m]")
+        plt.ylabel("Amplitude des vibrations [mm/s]")
+        plt.suptitle("a)")
+        plt.grid(True, which='major', color='k', alpha=.35)
+        plt.grid(True, which='minor', linestyle='--', color='k', alpha=.1)
+        plt.minorticks_on()
+        plt.tight_layout(rect=[0, 0, 1, 0.95])
+
+
+class Vitesse(Figure):
+    Metadata = Metadata
+
+    def plot(self, data):
+        plt.scatter(data["MPH"], data["RMS"], s=12, c="k")
+        plt.xlabel("Vitesse [mph]")
+        plt.ylabel("Amplitude des vibrations [mm/s]")
+        plt.suptitle("b)")
+        plt.grid(True, which='major', color='k', alpha=.35)
+        plt.grid(True, which='minor', linestyle='--', color='k', alpha=.1)
+        plt.minorticks_on()
+        plt.tight_layout(rect=[0, 0, 1, 0.95])
+
+
+class Poids(Figure):
+    Metadata = Metadata
+
+    def plot(self, data):
+        plt.scatter(data["Poids"], data["RMS"], s=12, c="k")
+        plt.xlabel("Poids [tonnes]")
+        plt.ylabel("Amplitude des vibrations [mm/s]")
+        plt.suptitle("c)")
+        plt.grid(True, which='major', color='k', alpha=.35)
+        plt.grid(True, which='minor', linestyle='--', color='k', alpha=.1)
+        plt.minorticks_on()
+        plt.tight_layout(rect=[0, 0, 1, 0.95])
+
+
+catalog.register(Distance)
+catalog.register(Vitesse)
+catalog.register(Poids)
