e31c70831a3dbe76710363994ac0133da229c783
CloudyOverhead <jerome.simon@geolearn.ca>
2022-03-09 11:08:03 -0500

    Add list argument to show registered figures

--- figures/__main__.py
+++ figures/__main__.py
@@ -6,20 +6,25 @@ from catalog import catalog
 
 
 parser = ArgumentParser()
+parser.add_argument('--list', '-l', action='store_true')
 parser.add_argument('--metadata', '-m', action='store_true')
 parser.add_argument('--figure', '-f', type=str)
 parser.add_argument('--show', '-s', action='store_true')
 args = parser.parse_args()
 
-if args.metadata:
-    if args.figure:
-        catalog.regenerate(args.figure)
-    else:
-        catalog.regenerate_all()
 
-if args.figure:
-    figure = catalog[args.figure]
-    figure.generate()
-    figure.save(show=args.show)
+if args.list:
+    print(catalog.filenames)
 else:
-    catalog.draw_all(show=args.show)
+    if args.metadata:
+        if args.figure:
+            catalog.regenerate(args.figure)
+        else:
+            catalog.regenerate_all()
+
+    if args.figure:
+        figure = catalog[args.figure]
+        figure.generate()
+        figure.save(show=args.show)
+    else:
+        catalog.draw_all(show=args.show)
