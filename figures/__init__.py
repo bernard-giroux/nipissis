dc8f6c21a421b18c790df6a7e56f8a118548d931
CloudyOverhead <jerome.simon@geolearn.ca>
2022-03-09 12:46:07 -0500

    Add probability example

--- figures/__init__.py
+++ figures/__init__.py
@@ -4,3 +4,4 @@ from . import location
 from . import histograms
 from . import inputs
 from . import dependencies
+from . import example_probability
---
+++ figures/example_probability.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import proplot as pplt
+
+from bayesian_inference import gaussian
+from dependencies import Dependencies_
+from catalog import catalog, Figure
+
+
+class ExampleProbabilities_(Dependencies_):
+    D = 90
+    V = 30
+    W = 20000
+
+    @property
+    def X(self):
+        return np.array([self.D, self.V, self.W, 1])
+
+    def generate(self):
+        super().generate()
+        vars = self["vars"]
+        self['rms'] = rms = np.linspace(0, 400, 48)
+
+        vars = np.array(np.meshgrid(*vars, copy=False, indexing='ij'))
+
+        rms = np.expand_dims(rms, tuple(range(1, len(vars)+1)))
+        mean = np.einsum('i,i...', self.X, vars[:-1])
+        mean = mean[None]
+        noise = vars[-1]
+        noise = noise[None]
+
+        prob = gaussian(x=rms, mean=mean, std=noise)
+        prob /= np.sum(prob)
+
+        self['prob_rms'] = prob
+
+
+class ExampleProbabilities(Figure):
+    Metadata = ExampleProbabilities_
+
+    def plot(self, data):
+        rms = data['rms']
+        prob = data['prob_rms']
+        prob = np.sum(prob, axis=tuple(range(1, prob.ndim)))
+        prob = np.cumsum(prob[::-1])[::-1]
+        self['prob_rms'] = prob
+
+        _, ax = pplt.subplots(figsize=[3.33, 3.33])
+
+        ax.plot(rms, prob, c='k')
+        ax.format(
+            xlabel="Amplitude threshold $y_t$ (mm/s)",
+            ylabel="Probability $p(y > y_t | \\bar{x})$ (―)",
+            xlim=[0, rms.max()],
+            ylim=[0, 1],
+        )
+
+
+catalog.register(ExampleProbabilities)
